9e8925f11ee7e62646fb4a363c749cb50fc3bca9
aschusumano <antonino1808@icloud.com>
2025-05-17 20:19:41 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [], // oder z.B. ["cdn.supabase.io"] falls du Bilder nutzt
+  },
 };
 
 export default nextConfig;
